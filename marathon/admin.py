f57ee3113f4b9793103389b84abb67705d61a79d
0chuhh <vvlasovdo@mail.ru>
2023-11-10 14:11:08 +0900

    (ref) admin

--- marathon/admin.py
+++ marathon/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from accounts.models import User
+from charitable_organization.models import CharitableOrg
+from inventory.models import Inventory
+from .models import Marathon,Checkpoint,Runners,Route
+from sponsorship.models import Sponsorship
 
 admin.site.register(User)
+admin.site.register(CharitableOrg)
+admin.site.register(Inventory)
+admin.site.register(Marathon)
+admin.site.register(Checkpoint)
+admin.site.register(Runners)
+admin.site.register(Route)
+admin.site.register(Sponsorship)
