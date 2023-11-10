47865ae7d4b4da509e2aff8996978c9b2ce65bad
0chuhh <vvlasovdo@mail.ru>
2023-11-10 09:28:33 +0900

    (add) accounts base model

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/accounts/avatars/', blank=True, default='')
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.middle_name} {self.last_name}'.replace('  ',' ')
+    
+    def __str__(self):
+        return f'{self.id} - {self.full_name}'
---
+++ accounts/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .forms import 
+
+# class LoginView()
